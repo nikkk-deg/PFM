6a0736561ea1288c9268125a6b4df9d03ff832d0
notm1nd <nikita.deki4@gmail.com>
2022-01-31 11:26:52 +0300

    i've refactored day 19

---
+++ #19/ToDo.js
@@ -0,0 +1,57 @@
+const STATUS = {
+    TO_DO: 'To Do',
+    IN_PROGRESS: 'In Progress',
+    DONE: 'Done',
+}
+
+const DEFAULT = STATUS.TO_DO;
+
+const list = {
+    "create a new practice task": "In Progress",
+    "make a bed": "Done",
+    "write a post": "ToDo",
+}
+
+function changeStatus(name, status){//name is better then key
+    if (name in list){
+        list[name] = status;
+    }
+}
+
+function addTask(name){//checking that the task already in list
+    if (!(name in list)){
+        list[name] = DEFAULT;
+    }
+}
+
+function deleteTask(name){
+    if (name in list){
+        delete list[name];
+    }
+}
+
+function showList(){
+    const tasks = {
+        [STATUS.TO_DO]: '',
+        [STATUS.IN_PROGRESS]: '',
+        [STATUS.DONE]: '',
+    }
+
+    for (let key in list) {
+        tasks[list[key]] += "  " + key + "\n";
+    }
+
+    if (tasks[STATUS.TO_DO] === ""){
+        tasks[STATUS.TO_DO] = ' -';
+    }
+    if (tasks[STATUS.IN_PROGRESS] === ""){
+        tasks[STATUS.IN_PROGRESS] = ' -';
+    }
+    if (tasks[STATUS.IN_PROGRESS] === ""){
+        tasks[STATUS.DONE] = ' -';
+    }
+
+    console.log(`${STATUS.TO_DO}:\n${tasks[STATUS.TO_DO]}\n${STATUS.IN_PROGRESS}:\n${tasks[STATUS.IN_PROGRESS]}\n${STATUS.DONE}:\n${tasks[STATUS.DONE]}`);
+}
+
+showList();
