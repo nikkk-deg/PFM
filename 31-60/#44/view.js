1ba1e122f143fe3df794b2e62ea67b0a4d836a2b
notm1nd <nikita.deki4@gmail.com>
2022-03-08 14:47:39 +0300

    add 44 day, all in functions, all works just for high priority taskx

---
+++ 31-60/#44/view.js
@@ -0,0 +1,15 @@
+export const UI_ElEMENTS = {
+    MAIN_CONTENT: document.querySelector('#main_content'),
+    ARTICLE_OF_FORM: document.querySelectorAll('.article_of_form'),
+    FORM_ADD_TASK: document.querySelectorAll('.form_add_task'),
+    INPUT_FIELD: document.querySelectorAll('.input_field'),
+    ADD_BUTTON: document.querySelectorAll('.add_button'),
+    ADD_HIGH_BUTTON: document.getElementById('high_add_button'),
+    ADD_LOW_BUTTON: document.getElementById('low_add_button'),
+    HIGH_FORM: document.querySelector('#high_form'),
+    LOW_FORM: document.querySelector('#low_form'),
+    HIGH_INPUT_FIELD: document.querySelector('#high_input_field'),
+    LOW_INPUT_FIELD: document.querySelector('#low_input_field'),
+    DELETE_BUTTON: document.querySelectorAll('.delete_button'),
+
+}
