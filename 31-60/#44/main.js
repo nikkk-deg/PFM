facef54444d255cdd21442b05399f9cba4934ba6
notm1nd <nikita.deki4@gmail.com>
2022-03-12 17:03:09 +0300

    fineshed work on toDo list. OMG it was so...strange?)

--- 31-60/#44/main.js
+++ 31-60/#44/main.js
@@ -3,30 +3,30 @@ import {UI_ElEMENTS} from "./view.js";
 const ELEMENT_HEIGHT = {
     HIGH_FORM_HEIGHT: 45,
     LOW_FORM_HEIGHT: 100,
-    LOW_BUTTON_ADD: 220,
+    LOW_ADD_BUTTON: 220,
 }
 
-const CHANGE_HEIGHT_NUMBERS = {
+const CHANGE_HEIGHT = {
+    LARGE_TASK: 100,
+    MIDDLE_TASK: 85,
+    LOW_TASK: 70,
 
-    SUPER_LARGE_CHANGE: 55,
-    LARGE_CHANGE: 40,
-    MIDDLE_CHANGE: 25,
+    LARGE_INCREASE: 110,
+    MIDDLE_INCREASE: 95,
+    LOW_INCREASE: 80,
 
-    SUPER_LARGE: 100,
-    LARGE: 85,
-    MIDDLE: 70,
+    LARGE_DECREASE: 110,
+    MIDDLE_DECREASE: 95,
+    LOW_DECREASE: 80,
 
-    SUPER_LARGE_DECREASE: 118,
-    LARGE_DECREASE: 103,
-    MIDDLE_DECREASE: 88,
-    ADD_TASK: 63,
-    NEW_TASK_HEIGHT: 45,
+    NORMAL_TASK: 45,
+    NORMAL_CHANGE_HEIGHT: 63,
 }
 
 const LENGTH_OF_INPUT_FIELD = {
-    X_LARGE: 90,
-    LARGE: 60,
-    MIDDLE: 30,
+    LARGE: 90,
+    MIDDLE: 60,
+    LOW: 30,
 }
 
 const IMAGES = {
@@ -71,65 +71,123 @@ function createCustomCheckbox(){
     return customCheckBox;
 }
 
-function createText(){
-    let text = document.createElement('p');
-    text.textContent = UI_ElEMENTS.HIGH_INPUT_FIELD.value;
+function createText(addedTask, form){
+    let text = document.createElement('pre');
+    text.textContent = form.value;
     text.className = CLASS_NAMES.TEXT;
+    if (form === UI_ElEMENTS.HIGH_INPUT_FIELD){
+        increaseHighTaskHeight(text.textContent, addedTask);
+    }else{
+        increaseLowTaskHeight(text.textContent, addedTask);
+    }
     return text;
 }
 
-function increaseAddedTask(heightOfAddedTask, changeHighHeight, addedTask){
-    addedTask.style = `height: ${heightOfAddedTask}px`;
-    ELEMENT_HEIGHT.HIGH_FORM_HEIGHT += changeHighHeight;
+function increaseHighForm(height){
+    ELEMENT_HEIGHT.HIGH_FORM_HEIGHT += height;
     UI_ElEMENTS.HIGH_FORM.style = `height: ${ELEMENT_HEIGHT.HIGH_FORM_HEIGHT}px`;
-    ELEMENT_HEIGHT.LOW_BUTTON_ADD += changeHighHeight;
-    UI_ElEMENTS.ADD_LOW_BUTTON.style = `top: ${ELEMENT_HEIGHT.LOW_BUTTON_ADD}px`;
+
+    ELEMENT_HEIGHT.LOW_ADD_BUTTON += height;
+    UI_ElEMENTS.ADD_LOW_BUTTON.style = `top: ${ELEMENT_HEIGHT.LOW_ADD_BUTTON}px`;
 }
 
-function decreaseField(changeHighHeight){
-    ELEMENT_HEIGHT.HIGH_FORM_HEIGHT -= changeHighHeight;
-    UI_ElEMENTS.HIGH_FORM.style = `height: ${ELEMENT_HEIGHT.HIGH_FORM_HEIGHT}px`;
-    ELEMENT_HEIGHT.LOW_BUTTON_ADD -= changeHighHeight;
-    UI_ElEMENTS.ADD_LOW_BUTTON.style = `top: ${ELEMENT_HEIGHT.LOW_BUTTON_ADD}px`;
+function increaseLowForm(changeHeight){
+    ELEMENT_HEIGHT.LOW_FORM_HEIGHT += changeHeight;
+    UI_ElEMENTS.LOW_FORM.style = `height: ${ELEMENT_HEIGHT.LOW_FORM_HEIGHT}px`;
 }
 
-function increaseHeightNewTask(src, addedTask){
-    if (src.length > LENGTH_OF_INPUT_FIELD.X_LARGE){
-        increaseAddedTask(CHANGE_HEIGHT_NUMBERS.SUPER_LARGE, CHANGE_HEIGHT_NUMBERS.SUPER_LARGE_CHANGE, addedTask)
-    }
-    else if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
-        increaseAddedTask(CHANGE_HEIGHT_NUMBERS.LARGE, CHANGE_HEIGHT_NUMBERS.LARGE_CHANGE, addedTask)
+function increaseTask(taskHeight, addedTask){
+    addedTask.style = `height: ${taskHeight}px`;
+}
+
+function increaseHighTaskHeight(src, addedTask){
+    if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
+        increaseTask(CHANGE_HEIGHT.LARGE_TASK, addedTask);
+        increaseHighForm(CHANGE_HEIGHT.LARGE_INCREASE);
     }
     else if (src.length > LENGTH_OF_INPUT_FIELD.MIDDLE){
-        increaseAddedTask(CHANGE_HEIGHT_NUMBERS.MIDDLE, CHANGE_HEIGHT_NUMBERS.MIDDLE, addedTask)
+        increaseTask(CHANGE_HEIGHT.MIDDLE_TASK, addedTask);
+        increaseHighForm(CHANGE_HEIGHT.MIDDLE_INCREASE);
+    }
+    else if (src.length > LENGTH_OF_INPUT_FIELD.LOW){
+        increaseTask(CHANGE_HEIGHT.LOW_TASK, addedTask);
+        increaseHighForm(CHANGE_HEIGHT.LOW_INCREASE);
+    }else{
+        increaseTask(CHANGE_HEIGHT.NORMAL_TASK, addedTask);
+        increaseHighForm(CHANGE_HEIGHT.NORMAL_CHANGE_HEIGHT);
     }
 }
 
-function decreaseHeightField(src){
-    if (src.length > LENGTH_OF_INPUT_FIELD.X_LARGE){
-        decreaseField(CHANGE_HEIGHT_NUMBERS.SUPER_LARGE_DECREASE)
-    }
-    else if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
-        decreaseField(CHANGE_HEIGHT_NUMBERS.LARGE_DECREASE)
+function increaseLowTaskHeight(src, addedTask){
+    if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
+        increaseTask(CHANGE_HEIGHT.LARGE_TASK, addedTask);
+        increaseLowForm(CHANGE_HEIGHT.LARGE_INCREASE);
     }
     else if (src.length > LENGTH_OF_INPUT_FIELD.MIDDLE){
-        decreaseField(CHANGE_HEIGHT_NUMBERS.MIDDLE_DECREASE)
+        increaseTask(CHANGE_HEIGHT.MIDDLE_TASK, addedTask);
+        increaseLowForm(CHANGE_HEIGHT.MIDDLE_INCREASE);
     }
-    else{
-        decreaseField(CHANGE_HEIGHT_NUMBERS.ADD_TASK)
+    else if (src.length > LENGTH_OF_INPUT_FIELD.LOW){
+        increaseTask(CHANGE_HEIGHT.LOW_TASK, addedTask);
+        increaseLowForm(CHANGE_HEIGHT.LOW_INCREASE);
+    }else {
+        increaseLowForm(CHANGE_HEIGHT.NORMAL_CHANGE_HEIGHT);
     }
 }
 
-function removeTask(arr){
+function decreaseHighFormHeight(height){
+    ELEMENT_HEIGHT.HIGH_FORM_HEIGHT -= height;
+    UI_ElEMENTS.HIGH_FORM.style = `height: ${ELEMENT_HEIGHT.HIGH_FORM_HEIGHT}px`;
+
+    ELEMENT_HEIGHT.LOW_ADD_BUTTON -= height;
+    UI_ElEMENTS.ADD_LOW_BUTTON.style = `top: ${ELEMENT_HEIGHT.LOW_ADD_BUTTON}px`;
+}
+
+function decreaseLowFormHeight(height) {
+    ELEMENT_HEIGHT.LOW_FORM_HEIGHT -= height;
+    UI_ElEMENTS.LOW_FORM.style = `height: ${ELEMENT_HEIGHT.LOW_FORM_HEIGHT}px`;
+}
+
+function decreaseField(src, form){
+    if (form === UI_ElEMENTS.HIGH_FORM){
+        if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
+            decreaseHighFormHeight(CHANGE_HEIGHT.LARGE_DECREASE)
+        }
+        else if (src.length > LENGTH_OF_INPUT_FIELD.MIDDLE){
+            decreaseHighFormHeight(CHANGE_HEIGHT.MIDDLE_DECREASE)
+        }
+        else if (src.length > LENGTH_OF_INPUT_FIELD.LOW){
+            decreaseHighFormHeight(CHANGE_HEIGHT.LOW_DECREASE)
+        }
+        else{
+            decreaseHighFormHeight(CHANGE_HEIGHT.NORMAL_CHANGE_HEIGHT)
+        }
+    }else{
+        if (src.length > LENGTH_OF_INPUT_FIELD.LARGE){
+            decreaseLowFormHeight(CHANGE_HEIGHT.LARGE_DECREASE)
+        }
+        else if (src.length > LENGTH_OF_INPUT_FIELD.MIDDLE){
+            decreaseLowFormHeight(CHANGE_HEIGHT.MIDDLE_DECREASE)
+        }
+        else if (src.length > LENGTH_OF_INPUT_FIELD.LOW){
+            decreaseLowFormHeight(CHANGE_HEIGHT.LOW_DECREASE)
+        }
+        else{
+            decreaseLowFormHeight(CHANGE_HEIGHT.NORMAL_CHANGE_HEIGHT)
+        }
+    }
+}
+
+function removeTask(arr, form){
     for (let element of arr) {
         element.firstChild.addEventListener('click', () => {
-            UI_ElEMENTS.HIGH_FORM.removeChild(element);
-            decreaseHeightField(element.lastChild.textContent);
+            form.removeChild(element);
+            decreaseField(element.lastChild.textContent, form);
         })
     }
 }
 
-function changeColorOfSelectedTask(arr){
+function changeColorOfSelectedTask(arr) {
     for (let element of arr) {
         element.firstChild.nextSibling.addEventListener('click', () => {
             if (element.firstChild.nextSibling.checked){
@@ -141,60 +199,62 @@ function changeColorOfSelectedTask(arr){
     }
 }
 
-function changeInputFieldColor(){
-    setTimeout(() => UI_ElEMENTS.HIGH_INPUT_FIELD.style = `background: ${COLORS.ERROR_INPUT}`);
-    setTimeout(() => UI_ElEMENTS.HIGH_INPUT_FIELD.style = `background: ${COLORS.WHITE}`, 200);
+function changeInputFieldColor(element){
+    setTimeout(() => element.style = `background: ${COLORS.ERROR_INPUT}`);
+    setTimeout(() => element.style = `background: ${COLORS.WHITE}`, 200);
 }
 
-function createTask(){
-    let addedTask = document.createElement('div');
+function createTask(form){
+    let addedTask = document.createElement('div');//!
     addedTask.className = CLASS_NAMES.NEW_TASK;
-    addedTask.style = `height: ${CHANGE_HEIGHT_NUMBERS.NEW_TASK_HEIGHT}`;
+    addedTask.append(createDeleteButton(),createCheckbox(),createCustomCheckbox(),createText(addedTask, form));
     return addedTask;
 }
 
-function changeHighFieldHeight(){
-    ELEMENT_HEIGHT.HIGH_FORM_HEIGHT += CHANGE_HEIGHT_NUMBERS.ADD_TASK;
-    ELEMENT_HEIGHT.LOW_BUTTON_ADD += CHANGE_HEIGHT_NUMBERS.ADD_TASK;
-    UI_ElEMENTS.HIGH_FORM.style = `height: ${ELEMENT_HEIGHT.HIGH_FORM_HEIGHT}px`;
-    UI_ElEMENTS.ADD_LOW_BUTTON.style = `top: ${ELEMENT_HEIGHT.LOW_BUTTON_ADD}px`;
+function checkValidInput(src){
+    if (src === ''){
+        return false
+    }
+    for (let element of src) {
+        if (element !== ' '){
+            return true;
+        }
+    }
+    return false;
 }
 
-
-function addTask(){
-    let validInput = UI_ElEMENTS.HIGH_INPUT_FIELD.value !== '';
-
-    if (validInput){
-        let deleteButton = createDeleteButton();
-        let checkbox = createCheckbox();
-        let customCheckbox = createCustomCheckbox();
-        let text = createText();
-        let addedTask = createTask();
-
+function addHighTask(){
+    if (checkValidInput(UI_ElEMENTS.HIGH_INPUT_FIELD.value)){
+        let addedTask = createTask(UI_ElEMENTS.HIGH_INPUT_FIELD);
         checkboxCount++;
-
-        increaseHeightNewTask(text.textContent, addedTask)
-
-        addedTask.append(deleteButton);
-        addedTask.append(checkbox);
-        addedTask.append(customCheckbox);
-        addedTask.append(text);
-
         UI_ElEMENTS.HIGH_FORM.append(addedTask);
-
-        changeHighFieldHeight();
-
         arrayOfAdddedTask.push(addedTask);
-
-        removeTask(arrayOfAdddedTask);
-
+        removeTask(arrayOfAdddedTask, UI_ElEMENTS.HIGH_FORM);
         changeColorOfSelectedTask(arrayOfAdddedTask);
-
     }else{
-        changeInputFieldColor();
+        changeInputFieldColor(UI_ElEMENTS.HIGH_INPUT_FIELD);
     }
+
     UI_ElEMENTS.HIGH_INPUT_FIELD.value = null;
 }
 
-UI_ElEMENTS.ADD_HIGH_BUTTON.addEventListener('click', addTask);
-// UI_ElEMENTS.ADD_LOW_BUTTON.addEventListener('click', addTask);
+function addLowTask(){
+    if (checkValidInput(UI_ElEMENTS.LOW_INPUT_FIELD.value)){
+        let addedTask = createTask(UI_ElEMENTS.LOW_INPUT_FIELD);
+        checkboxCount++;
+        UI_ElEMENTS.LOW_FORM.append(addedTask);
+        arrayOfAdddedTask.push(addedTask);
+        removeTask(arrayOfAdddedTask, UI_ElEMENTS.LOW_FORM);
+        changeColorOfSelectedTask(arrayOfAdddedTask);
+    }else{
+        changeInputFieldColor(UI_ElEMENTS.LOW_INPUT_FIELD);
+    }
+
+    UI_ElEMENTS.LOW_INPUT_FIELD.value = null;
+}
+
+UI_ElEMENTS.ADD_HIGH_BUTTON.addEventListener('click', addHighTask);
+
+UI_ElEMENTS.ADD_LOW_BUTTON.addEventListener('click', addLowTask);
+
+
